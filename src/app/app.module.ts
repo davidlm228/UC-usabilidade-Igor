efd81ce9a01c8acacbb6172aa76defdf2724c9a7
David Martins <davidlopes228@hotmail.com>
2020-10-02 18:31:18 -0300

    Commit - Portal de viagens - David Lopes Martins RA: 819165412

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
